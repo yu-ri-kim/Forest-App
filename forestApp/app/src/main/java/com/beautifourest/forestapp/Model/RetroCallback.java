3f66ffd03f4df34818590c484d91fb621e23830a
김유리 <yu-ri-@naver.com>
2020-07-25 21:58:33 +0900

    delete

--- forestApp/app/src/main/java/com/beautifourest/forestapp/Model/RetroCallback.java
+++
@@ -1,7 +0,0 @@
-package com.beautifourest.forestapp.Model;
-
-public interface RetroCallback<T> {
-    void onError(Throwable t);
-    void onSuccess(int code, T receivedData);
-    void onFailure(int code);
-}
